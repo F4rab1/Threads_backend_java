d22c30814324e12ddef9c5a56df17e1418df8397
F4rab1 <issafarabi2022@gmail.com>
2026-02-27 16:36:28 +0100

    Validating Thread Creation

--- src/main/java/com/farabi/threads/users/RegisterUserRequestDto.java
+++ src/main/java/com/farabi/threads/users/RegisterUserRequestDto.java
@@ -1,10 +1,20 @@
 package com.farabi.threads.users;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterUserRequestDto {
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must be less than 100 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
