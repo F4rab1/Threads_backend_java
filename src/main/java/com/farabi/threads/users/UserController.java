bc256ad0d9f13d85937a54da61acb3dc8f24f84e
F4rab1 <issafarabi2022@gmail.com>
2026-03-02 14:50:37 +0100

    Hashing Password

--- src/main/java/com/farabi/threads/users/UserController.java
+++ src/main/java/com/farabi/threads/users/UserController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -22,6 +23,7 @@ import java.util.Set;
 public class UserController {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping
     public ResponseEntity<List<UserResponseDto>> getAllUsers(@RequestParam(required = false, defaultValue = "", name = "sort") String sortBy) {
@@ -58,6 +60,7 @@ public class UserController {
         }
 
         var user = userMapper.toEntity(requestDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
 
         var userResponseDto = userMapper.toDto(user);
